a8788e68d891a155126180036339c0ace6219d0a
Jim Xue <develorcer@gmail.com>
2014-08-24 21:26:53 +0800

    Version 0.4.1

--- fs/exec.c
+++ fs/exec.c
@@ -155,7 +155,7 @@ PUBLIC int do_exec(MESSAGE * msg)
     struct proc * p = proc_table + src;
 
     /** wait for process to get ready, this is because 
-     *  send_recv(BOTH) will be transform into a msg_send
+     *  send_recv(BOTH) will be transformed into a msg_send
      *  following by a msg_receive, if we wipe out the image
      *  before msg_receive is called, we cannot send
      *  the message back to the process */
