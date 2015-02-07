fffff23dc6e1ad5892f13ef1b94351b423ed96bf
Jim Xue <develorcer@gmail.com>
2015-02-07 17:39:38 +0800

    Port newlib to arm-elf-lyos

--- scripts/build-newlib.sh
+++ scripts/build-newlib.sh
@@ -4,6 +4,28 @@ if [ -f /etc/debian_version ]; then
     sudo apt-get install python nasm build-essential wget libmpfr-dev libmpc-dev libgmp3-dev
 fi
 
+SUBARCH=$(uname -m | sed -e s/sun4u/sparc64/ \
+		-e s/arm.*/arm/ -e s/sa110/arm/ \
+		-e s/s390x/s390/ -e s/parisc64/parisc/ \
+		-e s/ppc.*/powerpc/ -e s/mips.*/mips/ \
+		-e s/sh[234].*/sh/ )
+ARCH=${SUBARCH}
+
+while getopts "m:" arg
+do
+        case $arg in
+             m)
+                ARCH=${OPTARG}
+                ;;
+             ?)
+                echo "unkonw argument"
+                exit 1
+                ;;
+        esac
+done
+
+export ARCH=${ARCH}
+
 pushd toolchain > /dev/null
 python build-newlib.py
 popd > /dev/null
