c9c65fd5b0c9e321ed57cc14f1b8fa79f44818ba
Jin Xue <csjinxue@outlook.com>
2021-07-24 19:40:20 +0800

    toolchain: add t-libgcc-pic to riscv cross gcc

--- toolchain/patches/newlib/lyos/syscalls.c
+++ toolchain/patches/newlib/lyos/syscalls.c
@@ -907,7 +907,7 @@ int open(const char* pathname, int flags, ...)
     return openat(AT_FDCWD, pathname, flags);
 }
 
-int read(int fd, void* buf, size_t count)
+_READ_WRITE_RETURN_TYPE read(int fd, void* buf, size_t count)
 {
     MESSAGE msg;
     msg.type = READ;
@@ -1055,7 +1055,7 @@ int lstat(const char* path, struct stat* buf)
     return fstatat(AT_FDCWD, path, buf, AT_SYMLINK_NOFOLLOW);
 }
 
-int write(int fd, const void* buf, size_t count)
+_READ_WRITE_RETURN_TYPE write(int fd, const void* buf, size_t count)
 {
     MESSAGE msg;
     msg.type = WRITE;
