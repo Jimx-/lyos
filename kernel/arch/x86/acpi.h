5c82a6f20a0c606ccb1e08ccb31eecffae72183c
Jin Xue <csjinxue@outlook.com>
2021-12-19 18:14:12 +0800

    x86_64: add attribute packed to ACPI table structures

--- kernel/arch/x86/acpi.h
+++ kernel/arch/x86/acpi.h
@@ -9,7 +9,7 @@ struct acpi_rsdp {
     u8 revision;
     u32 rsdt_addr;
     u32 length;
-};
+} __attribute__((packed));
 
 #define ACPI_SDT_SIGNATURE_LEN 4
 
@@ -26,7 +26,7 @@ struct acpi_sdt_header {
     u32 oem_revision;
     u32 creator_id;
     u32 creator_revision;
-};
+} __attribute__((packed));
 
 struct acpi_generic_address {
     u8 address_space_id;
@@ -34,37 +34,37 @@ struct acpi_generic_address {
     u8 register_bit_offset;
     u8 access_size;
     u64 address;
-};
+} __attribute__((packed));
 
 struct acpi_madt_hdr {
     struct acpi_sdt_header hdr;
     u32 local_apic_address;
     u32 flags;
-};
+} __attribute__((packed));
 
-#define ACPI_MADT_TYPE_LAPIC 0
-#define ACPI_MADT_TYPE_IOAPIC 1
-#define ACPI_MADT_TYPE_INT_SRC 2
-#define ACPI_MADT_TYPE_NMI_SRC 3
-#define ACPI_MADT_TYPE_LAPIC_NMI 4
-#define ACPI_MADT_TYPE_LAPIC_ADRESS 5
-#define ACPI_MADT_TYPE_IOSAPIC 6
-#define ACPI_MADT_TYPE_LSAPIC 7
+#define ACPI_MADT_TYPE_LAPIC            0
+#define ACPI_MADT_TYPE_IOAPIC           1
+#define ACPI_MADT_TYPE_INT_SRC          2
+#define ACPI_MADT_TYPE_NMI_SRC          3
+#define ACPI_MADT_TYPE_LAPIC_NMI        4
+#define ACPI_MADT_TYPE_LAPIC_ADRESS     5
+#define ACPI_MADT_TYPE_IOSAPIC          6
+#define ACPI_MADT_TYPE_LSAPIC           7
 #define ACPI_MADT_TYPE_PLATFORM_INT_SRC 8
-#define ACPI_MADT_TYPE_Lx2APIC 9
-#define ACPI_MADT_TYPE_Lx2APIC_NMI 10
+#define ACPI_MADT_TYPE_Lx2APIC          9
+#define ACPI_MADT_TYPE_Lx2APIC_NMI      10
 
 struct acpi_madt_item_hdr {
     u8 type;
     u8 length;
-};
+} __attribute__((packed));
 
 struct acpi_madt_lapic {
     struct acpi_madt_item_hdr hdr;
     u8 acpi_cpu_id;
     u8 apic_id;
     u32 flags;
-};
+} __attribute__((packed));
 
 struct acpi_madt_ioapic {
     struct acpi_madt_item_hdr hdr;
@@ -72,7 +72,7 @@ struct acpi_madt_ioapic {
     u8 __reserved;
     u32 address;
     u32 global_int_base;
-};
+} __attribute__((packed));
 
 struct acpi_madt_int_src {
     struct acpi_madt_item_hdr hdr;
@@ -80,7 +80,7 @@ struct acpi_madt_int_src {
     u8 bus_int;
     u32 global_int;
     u16 mps_flags;
-};
+} __attribute__((packed));
 
 struct acpi_hpet {
     struct acpi_sdt_header hdr;
@@ -89,7 +89,7 @@ struct acpi_hpet {
     u8 hpet_number;
     u16 minimum_tick;
     u8 page_protection;
-};
+} __attribute__((packed));
 
 #define MAX_RSDT 35
 
