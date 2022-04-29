061fc44011c1f9deada4f86ce6214181e64f347d
Jin Xue <csjinxue@outlook.com>
2022-04-29 23:19:31 +0800

    strace: support arm64

--- utils/cmd-utils/strace/arch/arm64/arch_regs.c
+++ utils/cmd-utils/strace/arch/arm64/arch_regs.c
@@ -1,6 +1,16 @@
+#include <sys/ptrace.h>
+
 #include "../../types.h"
 #include "../../proto.h"
 
-void arch_get_syscall_args(struct tcb* tcp) {}
+void arch_get_syscall_args(struct tcb* tcp)
+{
+    tcp->u_args[0] =
+        ptrace(PTRACE_PEEKUSER, tcp->pid, (void*)(8 * 8), 0);        /* w8 */
+    tcp->u_args[1] = ptrace(PTRACE_PEEKUSER, tcp->pid, (void*)0, 0); /* x0 */
+}
 
-void arch_get_syscall_rval(struct tcb* tcp) {}
+void arch_get_syscall_rval(struct tcb* tcp)
+{
+    tcp->rval = ptrace(PTRACE_PEEKUSER, tcp->pid, (void*)0, 0); /* x0 */
+}
